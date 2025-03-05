4902d1d36d0dae09d6138aa7cdbb54267913c579
Fran Delgado <franciscodelgalop@gmail.com>
2025-03-05 10:46:14 -0300

    Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // 👈 Nombre exacto de tu repo
+  base: './', // 
   plugins: [react()],
 });
 
